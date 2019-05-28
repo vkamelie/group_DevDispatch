b57bbafab9a10489f763faf9b3c3acca74366bf9
= <=>
2019-05-28 16:09:27 -0700

    simple styling

--- src/Components/Review/Reviews.jsx
+++ src/Components/Review/Reviews.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import "./review.css";
 
 export default function Reviews(props) {
   return props.reviews.map(review => {
     return (
-      <span>
-        <h3>{review.username}</h3>
-        <h3>{review.course}</h3>
-        <h3>{review.title}</h3>
+      <div className="review">
+        <h5>{review.username}</h5>
+        <div>
+          <h3>{review.course}</h3>
+          <h3>{review.location}</h3>
+        </div>
         <h3>{review.description}</h3>
-        <h3>{review.location}</h3>
         <h3>{review.rate}</h3>
-      </span>
+      </div>
     );
   });
 }
