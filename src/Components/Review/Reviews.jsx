3f87bdff077c4b2d4a7f6c425de1ef0ad67382c3
= <=>
2019-05-30 14:42:10 -0700

    Do you really wanna hurt me? Do you really wanna make me cry?

--- src/Components/Review/Reviews.jsx
+++ src/Components/Review/Reviews.jsx
@@ -6,6 +6,7 @@ export default function Reviews(props) {
     return (
       <div className="review">
         <h5>{review.username}</h5>
+        <h5>{review.title}</h5>
         <div>
           <h3>{review.course}</h3>
           <h3>{review.location}</h3>
